86743e74b3d167e8d47d8b5a4c649d17b89eca62
jhovany moises lopez gomez <Jhovany27@gmail.com>
2025-04-25 23:59:11 -0600

    Actualización de proyecto

--- tienda-gamer/main/juegos.php
+++ tienda-gamer/main/juegos.php
@@ -19,7 +19,7 @@ $productos = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
@@ -81,7 +81,7 @@ $productos = [
                             <a class="nav-link me-md-2" href="#">EN DESCUENTO</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">NOTICIAS</a>
+                            <a class="nav-link" href="noticias.php">NOTICIAS</a>
                         </li>
                     </ul>
                     <form class="d-flex">
--- tienda-gamer/main/noticias.php
+++ tienda-gamer/main/noticias.php
@@ -20,7 +20,7 @@ Y ahora Miquella aguarda el retorno de su señor prometido..', 'imagen' => 'http
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Noticias</title>
 
 </head>
